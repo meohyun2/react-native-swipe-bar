4550b468fa4b3c5b5925efc80743c12e4e5d3773
meohyun2 <eogus6242@gmail.com>
2021-05-23 02:12:05 +0900

    fix: bone head play

--- src/SwipeButton.tsx
+++ src/SwipeButton.tsx
@@ -1,63 +1,77 @@
-import React, {useRef} from 'react';
+import React, {useRef, useMemo, useState, useEffect, useCallback} from 'react';
 
 import {Animated, View, StyleSheet, PanResponder, Text} from 'react-native';
 
-const SwipeButton = (): React.ReactElement => {
-  const pan = useRef(new Animated.Value()).current;
+import useLayout from './Hook/useLayout';
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderGrant: () => {
-        pan.setOffset({
-          x: pan.x._value,
-          y: pan.y._value
-        });
-      },
-      onPanResponderMove: Animated.event(
-        [
-          null,
-          {dx: pan.x, dy: pan.y}
-        ]
-      ),
-      onPanResponderRelease: () => {
-        pan.flattenOffset(); // slider 안걸리면 다시 초기화 하는 로직 필요
-      }
-    })
-  ).current;
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>Drag this box!</Text>
-      <Animated.View
-        style={{
-          transform: [{ translateX: pan.x }, { translateY: pan.y }]
-        }}
-        {...panResponder.panHandlers}
-      >
-        <View style={styles.box} />
-      </Animated.View>
-    </View>
-  )
+interface Props {
+  SwitchComponent?: React.ReactElement;
+  isCompleted?: boolean; // flag to define complete State.
+  CompletedComponent?: React.ReactElement; // if you want to render Another Component, pass the Component to this prop.
+  completeBaseLinePercentage: number; // percentage for container size, it will define complete event
+  onCompleted(): void; // callback when switch moved end
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  titleText: {
-    fontSize: 14,
-    lineHeight: 24,
-    fontWeight: "bold"
-  },
-  box: {
-    height: 150,
-    width: 150,
-    backgroundColor: "blue",
-    borderRadius: 5
-  }
-});
+const SwipeButton = ({
+  isCompleted = false,
+  SwitchComponent,
+  CompletedComponent,
+  completeBaseLinePercentage = 90,
+  onCompleted,
+}: Props): React.ReactElement => {
+  const [isComplete, setIsComplete] = useState<boolean>(isCompleted);
+  const pan = useRef(new Animated.ValueXY()).current;
+  const [containerSize, onLayout] = useLayout();
+  useEffect(() => {
+    console.log(isComplete);
+  }, [isComplete]);
+
+  const panResponder = !isComplete
+    ? PanResponder.create({
+        onMoveShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+          pan.setOffset({
+            x: pan.x._value,
+            y: pan.y._value,
+          });
+        },
+        onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}], {
+          useNativeDriver: false,
+        }),
+        onPanResponderRelease: (e, gesture) => {
+          if (
+            gesture.moveX >
+            (containerSize?.width ?? 1) * (completeBaseLinePercentage / 100)
+          ) {
+            onCompleted?.();
+            pan.setValue({
+              x:
+                (containerSize?.width ?? 1) *
+                (completeBaseLinePercentage / 100),
+              y: 0,
+            });
+            setIsComplete(true);
+          } else {
+            pan.setValue({
+              x: 0,
+              y: 0,
+            });
+          }
+        },
+      })
+    : null;
+
+  return (
+    <Animated.View
+      onLayout={onLayout}
+      style={{
+        transform: [{translateX: pan.x}, {translateY: 0}],
+      }}
+      {...(panResponder?.panHandlers ?? null)}>
+      {isComplete && CompletedComponent ? CompletedComponent : null}
+      {SwitchComponent}
+    </Animated.View>
+  );
+};
 
 export default SwipeButton;
